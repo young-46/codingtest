7cb44983b2b84b6ad20836101e031fea1f5533b5
young <71377091+young-46@users.noreply.github.com>
2022-11-30 03:35:00 +0900

    [Silver III] Title: 이상한 술집, Time: 8 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.cc
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.cc
@@ -14,7 +14,6 @@ int main()
         cin >> a;
         ml.push_back(a);
     }
-    sort(ml.begin(), ml.end());
     int pf = 0;
     long long pl = 2147483647;
     //int pl = ml[0]; // 왜 최대를 굳이 2^31 - 1로?
@@ -27,14 +26,12 @@ int main()
         for(int i = 0; i < n; i++){
             cnt += ml[i]/pk; // 전혀 생각하지 못한 코드
         }
-        if(cnt < k){
+        if(cnt < k){ // 크다
             pl = pk - 1;
-            // 너무 크다
         }
-        else{
-            result = pk;
-            pf = pk + 1;
-            // 너무 작다
+        else{ // 작다
+            result = pk; // cnt가 k보다 크니까 k명한테는 나누어 주었음 그 후 더 많은 용량을 나누어 줄 수 있는지 확인... 
+            pf = pk + 1;  
         }
     }
     cout << result;
